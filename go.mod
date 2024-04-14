008e00532dec685a94d5303597ecd53dad4721d4
Delevin Rero <delevinrero@gmail.com>
2024-04-14 14:48:51 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module website
+
+go 1.22.1
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ kodmain.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+var bot *tgbotapi.BotAPI
+
+func main() {
+
+	var err error
+	bot, err = tgbotapi.NewBotAPI("6740393670:AAHTCTwZ2wQQ9LL-YNTjfsBZ3KH38-Q4Los")
+	if err != nil {
+		log.Panic(err)
+	}
+	// Устанавливаем обработчики маршрутов
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/order", orderHandler) // Новый обработчик для маршрута /order
+	http.HandleFunc("/submit_order", submit)
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("HTML/html/css"))))
+	http.Handle("/newcss/", http.StripPrefix("/newcss/", http.FileServer(http.Dir("D:/golang/Todo/2/HTML/order/newcss"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("HTML/html/js"))))
+	http.Handle("/fonts/", http.StripPrefix("/fonts/", http.FileServer(http.Dir("HTML/html/fonts"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("HTML/html/img"))))
+	http.Handle("/2img/", http.StripPrefix("/2img/", http.FileServer(http.Dir("HTML/order/2img"))))
+
+	log.Println("Сервер запущен на порту 8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Ошибка при запуске сервера: %v", err)
+	}
+}
+
+// Обработчик для главной страницы
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "HTML/html/index.html")
+}
+
+func orderHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "HTML/order/order.html")
+}
+
+func submit(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "HTML/order/order.html")
+	name := r.FormValue("name")
+	zakaz := r.FormValue("zakaz")
+	adres := r.FormValue("adres")
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/china")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `orders` (`name`,`zakaz`,`adres`) VALUES('%s', '%s', '%s')", name, zakaz, adres))
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	orderText := fmt.Sprintf("**_Новый заказ!_**\n*Заказчик:* %s\n*Заказ:* %s\n*Адрес:* %s", name, zakaz, adres)
+
+	// Отправляем сообщение в телеграм
+	msg := tgbotapi.NewMessage(-1001596545488, orderText)
+	msg.ParseMode = "Markdown" // Устанавливаем режим разметки Markdown
+	_, err = bot.Send(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	// Перенаправляем пользователя
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
